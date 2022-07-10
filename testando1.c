eddd189d8d8e35a0c749b199d4ad9f3e094de22d
Brenow <brenowsuper50@gmail.com>
2022-07-10 19:02:35 -0300

    Versão 1.1

--- testando1.c
+++ testando1.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     int i;
+    char s[30];
+
+    printf("Digite o seu nome:\n");
+    gets(s);
     printf("Hello World!\n");
     i = 1 + 1;
-    printf("sua idade eh %d anos", i);
+    printf("sua idade eh %d anos\n", i);
+    printf("O seu nome eh %s\n", s);
 
     return 0;
 }
